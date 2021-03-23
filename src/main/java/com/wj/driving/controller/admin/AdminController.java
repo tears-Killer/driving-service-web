a70fe4fadbd2ce9b8856f034d9349b3104d4abd7
wangjian <913864980@qq.com>
2021-03-23 19:48:11 +0800

    代码修复

--- src/main/java/com/wj/driving/controller/admin/AdminController.java
+++ src/main/java/com/wj/driving/controller/admin/AdminController.java
@@ -4,6 +4,7 @@ import com.wj.driving.exceptions.MessageErrorCode;
 import com.wj.driving.model.admin.AdminUpdatePwdVO;
 import com.wj.driving.restfulapi.dto.admin.AdminDetailsDTO;
 import com.wj.driving.restfulapi.request.AdminSearchRequest;
+import com.wj.driving.restfulapi.request.admin.AdminSearchRequest;
 import com.wj.driving.result.BaseResult;
 import com.wj.driving.result.admin.AdminResult;
 import com.wj.driving.service.admin.AdminService;
